a6bf16646c765b343bb7a714e71d5f6ba3fa2a3f
Michael Jiang <jiangm1@yahoo.com>
2020-02-16 22:31:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,31 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  [ 1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["earth","water","fire"]
+  array = array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["earth","water","fire"]
+  array = array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  puts
 end
 
 def retrieve_first_element_from_array(array)
